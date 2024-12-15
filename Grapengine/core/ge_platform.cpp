17a13ff1d3354712a05e37cbd2bf9a27d784a2f9
Vinicius Almada <viniciusalmada10@gmail.com>
2024-12-15 00:57:09 -0300

    dev: remove  Platform::GetCurrentTimeMS

--- Grapengine/core/ge_platform.cpp
+++ Grapengine/core/ge_platform.cpp
@@ -2,18 +2,6 @@
 
 using namespace GE;
 
-u64 Platform::GetCurrentTimeMS()
-{
-  // Get the current time point
-  auto now = std::chrono::system_clock::now();
-
-  // Cast the time point to milliseconds since epoch
-  auto duration = now.time_since_epoch();
-  auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-
-  return static_cast<u64>(millis);
-}
-
 u64 Platform::GetCurrentTimeNS()
 {
   // Get the current time point
