9048fdefeff11d01200e22e988411ed2038446ce
Vini <viniciusalmada10@gmail.com>
2026-05-10 09:48:04 -0300

    test: split test project into unit tests and graphics tests

--- tests/EngineGraphicsTests/tests_pch.hpp
+++ tests/EngineGraphicsTests/tests_pch.hpp
@@ -5,6 +5,7 @@
 #include <any>
 #include <array>
 #include <cstdint>
+#include <deque>
 #include <fstream>
 #include <functional>
 #include <list>
---
+++ tests/EngineUnitTests/tests_pch.hpp
@@ -0,0 +1,21 @@
+#ifndef GRAPENGINE_TESTS_PCH_HPP
+#define GRAPENGINE_TESTS_PCH_HPP
+
+#include <algorithm>
+#include <any>
+#include <array>
+#include <cstdint>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <list>
+#include <map>
+#include <memory>
+#include <optional>
+#include <random>
+#include <ranges>
+#include <set>
+#include <source_location>
+#include <variant>
+
+#endif // GRAPENGINE_TESTS_PCH_HPP
